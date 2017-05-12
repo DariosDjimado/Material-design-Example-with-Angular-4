64b097db99b0cc8ba1b65a2e4bfd9db3f02c7afd
djicodas <comlanvi-darios.djimado@telecom-st-etienne.fr>
2017-05-12 16:32:46 +0200

    add cards, and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 // import material
 import { MaterialModule }  from '@angular/material';
@@ -9,16 +10,33 @@ import { MaterialModule }  from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { TemplatesComponent } from './components/templates/templates.component';
+import { HomeComponent } from './components/home/home.component';
+import { MyWorksComponent } from './components/my-works/my-works.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+const mainRoutes:  Routes=[
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path:'home', component: HomeComponent},
+  {path: 'templates', component: TemplatesComponent},
+  {path: 'works', component: MyWorksComponent},
+  {path:'**',component: PageNotFoundComponent}
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplatesComponent,
+    HomeComponent,
+    MyWorksComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(mainRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
