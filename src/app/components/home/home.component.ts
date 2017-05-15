bb7ad3c381e9daa0ee162e98973e320dcc289977
djicodas <comlanvi-darios.djimado@telecom-st-etienne.fr>
2017-05-15 15:41:44 +0200

    add brand description and remove opacity attribute on home img

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,8 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-	homeImgOpacity:number=0.9;
-	homeScale:number=2;
+	homeScale:number;
   	constructor() { }
 
 	ngOnInit() {
@@ -15,9 +14,7 @@ export class HomeComponent implements OnInit {
 			let scrollPositionY=window.scrollY;
 
 			if (scrollPositionY<400){
-
-				this.homeImgOpacity = Math.max(scrollPositionY*0.9/400,0.2);
-				this.homeScale=2-scrollPositionY*0.9/400;
+				this.homeScale=Math.max(scrollPositionY*0.9/300,1);
 			}
 			
 		})
