6b3e6b78259c1ba00d7a847ecae23e1d52d999fe
djicodas <comlanvi-darios.djimado@telecom-st-etienne.fr>
2017-05-18 20:20:35 +0200

    add grid list

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 	homeScale:number;
 	docu=false;
+
+	tiles=[
+		{header:'ANGULAR',img:'assets/img/angular.svg',footer:{company:'Google',tooltip:'', link:'#'}},
+		{header:'HTML5',img:'assets/img/HTML5_sticker.svg',footer:{company:'W3C',tooltip:'World Wide Web Consortium', link:'#'}},
+		{header:'NODE JS',img:'assets/img/nodejs-new-pantone-black.png',footer:{company:'Node.js',tooltip:'Node.js Foundation', link:'#'}},
+		{header:'WEBPACK',img:'assets/img/webpack-icon.svg',footer:{company:'Webpack',tooltip:'Webpack module bundler', link:'#'}}
+
+	];
+
   	constructor() { }
 
 	ngOnInit() {
